c1a000b7952c6ebec173c6bdcfe841a0e1b8be50
Gabi Hoffmann <hoffmann.agf@gmail.com>
2022-02-01 21:58:26 -0300

    feat: add type alias

--- types/union&aliases.ts
+++ types/union&aliases.ts
@@ -1,6 +1,12 @@
+type ID = number | string
 //A union is a type formed by two types or more
-function printId(id: number | string) {
+function printId(id: ID) {
   console.log(`Your ID is ${id}`);
 }
 printId(1)// Your ID is 1
 printId('1')// Your ID is 1
+
+//when use the same type more than once, use a refer by alias
+function printInfo(id: ID, item: string) {
+  console.log(`A product ${item} has ID: ${id}`)
+}
