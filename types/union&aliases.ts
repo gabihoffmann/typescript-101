3953e81b344f0a2303660fafd81572f3e2d77723
Gabi Hoffmann <hoffmann.agf@gmail.com>
2022-02-01 21:55:03 -0300

    feat: add type union

---
+++ types/union&aliases.ts
@@ -0,0 +1,6 @@
+//A union is a type formed by two types or more
+function printId(id: number | string) {
+  console.log(`Your ID is ${id}`);
+}
+printId(1)// Your ID is 1
+printId('1')// Your ID is 1
