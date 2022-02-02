ccfc48f4b8e91f06af87d41af313a6fc86422cda
Gabi Hoffmann <hoffmann.agf@gmail.com>
2022-02-01 22:16:14 -0300

    feat: add optional propertie

--- types/union&aliases.ts
+++ types/union&aliases.ts
@@ -1,7 +1,7 @@
 type ID = number | string
 //A union is a type formed by two types or more
 function printId(id: ID) {
-  console.log(`Your ID is ${id}`);
+  console.log(`Your ID is ${id}`)
 }
 printId(1)// Your ID is 1
 printId('1')// Your ID is 1
@@ -9,4 +9,23 @@ printId('1')// Your ID is 1
 //when use the same type more than once, use a refer by alias
 function printInfo(id: ID, item: string) {
   console.log(`A product ${item} has ID: ${id}`)
+}
+
+type Platform = 'Windows' | 'Linux' | 'MacOS'
+
+function renderPlatform(platform: Platform) {
+  return platform
+}
+
+renderPlatform('Windows')
+
+type AccountInfo = {
+  id: number,
+  name: string,
+  email?: string
+}
+// A optional properties means the type can be undefined
+const account: AccountInfo = {
+  id: 123,
+  name: "Gabi"
 }
