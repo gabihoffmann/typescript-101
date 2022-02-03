4b69f857c1960b8e68c14fb37297f1883bd56871
Gabi Hoffmann <hoffmann.agf@gmail.com>
2022-02-02 21:34:34 -0300

    feat: add classes extends

--- types/classes.ts
+++ types/classes.ts
@@ -16,3 +16,24 @@ const gabi = new UserAccount('Gabi', 29)
 console.log(gabi)
 console.log(gabi.name)
 gabi.logDetails()
+
+class CharAccount extends UserAccount {
+  nickname: string
+  level: number
+
+  constructor(nickname: string, level: number, name: string, age: number) {
+    super(name, age)
+    this.nickname = nickname
+    this.level = level
+  }
+
+  logLevel(): void {
+    console.log(`This player are ${this.nickname} and have ${this.level} xp`)
+  }
+}
+
+const hoffmann = new CharAccount('GabiHoffmann', 1000, 'Gabi', 29)
+console.log(hoffmann)
+console.log(hoffmann.nickname)
+hoffmann.logDetails()
+hoffmann.logLevel()
