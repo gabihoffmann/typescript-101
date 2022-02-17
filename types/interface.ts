62a21551d42107ca6c7a2ac29002758a2cdbf65b
Gabi Hoffmann <hoffmann.agf@gmail.com>
2022-02-16 22:36:04 -0300

    feat: add interface

---
+++ types/interface.ts
@@ -0,0 +1,21 @@
+//Interface
+interface Game {
+  title: string
+  description: string
+  genre: string
+  platform: string[]
+  getSimilar: (title: string) => void
+}
+
+const myGame: Game = {
+  title: 'Spider Solitaire',
+  description: 'A card game',
+  genre: 'paciente',
+  platform: ['web', 'windows'],
+  getSimilar: (title: string) => {
+    console.log(`Similar game to ${title}: Solitaire, `)
+  }
+}
+
+console.log(myGame.title)
+myGame.getSimilar(myGame.title)
