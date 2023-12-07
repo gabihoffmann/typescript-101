5a6199201b87c87e6a26bb67af9a3763d3726fc7
Gabi Hoffmann <hoffmann.agf@gmail.com>
2023-12-06 21:30:16 -0300

    fix: comment error case

--- types/primitives.ts
+++ types/primitives.ts
@@ -3,27 +3,27 @@
 // primitives
 
 // boolean (true | false)
-let isBoolean: boolean
-isBoolean = true
+let isBoolean: boolean;
+isBoolean = true;
 
 // string ( 'text', "text", `text`)
-let isString: string
-isString = `olá`
+let isString: string;
+isString = `olá`;
 
 // number (int, float, hex, binary)
-let isNumber: number
-isNumber = 0xff0
+let isNumber: number;
+isNumber = 0xff0;
 
 // array
-let isArray: number[]
-isArray = [0, 1, 2, 3, 4]
+let isArray: number[];
+isArray = [0, 1, 2, 3, 4];
 
-let anotherArray: Array<number>
-anotherArray = [4, 3, 2, 1, 0, 1]
+let anotherArray: Array<number>;
+anotherArray = [4, 3, 2, 1, 0, 1];
 
 // tuple
-let isTuple: [number, string, boolean]
-isTuple = [1, 'text', true]
+let isTuple: [number, string, boolean];
+isTuple = [1, "text", true];
 
 // enum
 enum isEnum {
@@ -31,32 +31,35 @@ enum isEnum {
   second = 2,
 }
 
-// any 
-// !!!Warning, this not cool 
-let anything: any
-anything = 0
-anything = 'a'
-anything = [1, 2, 3]
-anything = true
+// any
+// !!!Warning, this not cool
+let anything: any;
+anything = 0;
+anything = "a";
+anything = [1, 2, 3];
+anything = true;
 
 // void
 function willReturnVoid(): void {
-  console.log('this function does not return value')
+  console.log("this function does not return value");
 }
 
 // never
 function error(): never {
-  throw new Error('error')
+  throw new Error("error");
 }
 
 // object
-let isObject: object
+let isObject: object;
 isObject = {
-  key: 'value'
-}
+  key: "value",
+};
 
 // Type Inference
 // TS used inference to provide type information
-let newString = 'defined string'
-newString = 'new string value'
-newString = 1 
+let newString = "defined string";
+newString = "new string value";
+/**
+ * Type 'number' não pode ser atribuído ao tipo 'string'
+ */
+// newString = 1
